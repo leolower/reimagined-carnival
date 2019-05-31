e7be60903a183ce7b9ac9aade36c7428d66ca893
Sebastián Galiano <sgaliano@gmail.com>
2019-05-31 15:46:32 -0300

    Add missing typing

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,16 @@
 export * from './PolicyService';
+
+declare module '@makerdao-cdp-insurance/smart-contracts-abi' {
+  interface ContractArtifact {
+    contractName: string;
+    abi: any[];
+    networks: {
+      [networkId: string]: {
+        address: string;
+        transactionHash: string;
+        // ...
+      };
+    };
+    // ...
+  }
+}
