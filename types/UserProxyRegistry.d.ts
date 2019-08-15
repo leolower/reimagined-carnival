80d585deed2d4242debdff4b6e35b62045312a57
Lisandro Corbalan <lmcorbalan@gmail.com>
2019-08-15 08:34:09 -0300

    UserProxyRegistry add getAddressByProxy + new kovan address

--- types/UserProxyRegistry.d.ts
+++ types/UserProxyRegistry.d.ts
@@ -1,7 +1,9 @@
 import Web3 from 'web3';
+import { EventOptions } from 'web3-eth-contract';
 import SmartContract from './SmartContract';
 export declare class UserProxyRegistry extends SmartContract {
     static build(web3: Web3): Promise<UserProxyRegistry>;
     getUserProxies(owner: string): Promise<any>;
+    getAddressByProxy(options?: EventOptions): Promise<Map<string, string>>;
 }
 export default UserProxyRegistry;
