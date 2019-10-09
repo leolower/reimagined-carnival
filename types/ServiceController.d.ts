b559cfef0ff18308fab5d80651c77dbaa4eb4f58
Lisandro Corbalan <lmcorbalan@gmail.com>
2019-10-09 13:19:03 -0300

    Export some missing types

--- types/ServiceController.d.ts
+++ types/ServiceController.d.ts
@@ -1,6 +1,6 @@
 import Web3 from 'web3';
 import SmartContract from './SmartContract';
-declare class ServiceController extends SmartContract {
+export declare class ServiceController extends SmartContract {
     static build(web3: Web3): Promise<ServiceController>;
     getServiceAddress(service: string): Promise<any>;
 }
